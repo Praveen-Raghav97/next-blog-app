5d73623b9350fbf609010f377890708fa38251e7
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-07-04 12:08:23 +0530

    chnge

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -28,16 +28,10 @@ const Header = () => {
    }
  }
   return (
-    <AuroraBackground>
+    <>
       
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.1,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
+      <div
+        
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
         <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">Your words can change the world.</div>
@@ -56,9 +50,9 @@ const Header = () => {
         </button>
        </form>
        
-      </motion.div>
+      </div>
     
-    </AuroraBackground>
+    </>
     
   );
 };
